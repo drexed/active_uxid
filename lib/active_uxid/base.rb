db3332b22b4e2a81501a6e1d404378d275851f4a
Juan Gomez <j.gomez@drexed.com>
2017-10-04 14:49:41 -0400

    retry

--- lib/active_uxid/base.rb
+++ lib/active_uxid/base.rb
@@ -4,7 +4,7 @@ module ActiveUxid
   class Base
 
     ENCODING_CHARS ||= ActiveUxid.configuration.encoding_chars
-    ENCODING_LENGTH ||= ActiveUxid.configuration.encoding_length
+    ENCODING_LENGTH = ActiveUxid.configuration.encoding_length
     ENCODING_SALT ||= ActiveUxid.configuration.encoding_salt
 
   end

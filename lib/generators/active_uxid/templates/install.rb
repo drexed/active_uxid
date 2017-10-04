3235c5959d6b589f7342dd0a8d02192ffea3cfe6
Juan Gomez <j.gomez@drexed.com>
2017-10-04 09:21:46 -0400

    clean up

--- lib/generators/active_uxid/templates/install.rb
+++ lib/generators/active_uxid/templates/install.rb
@@ -4,5 +4,5 @@ ActiveUxid::Settings.configure do |config|
   config.encoder_type = 'ulid'
   config.encoding_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
   config.encoding_length = 26
-  config.encoding_length = 1369136
+  config.encoding_salt = 1369136
 end
--- spec/lib/active_uxid/settings_spec.rb
+++ spec/lib/active_uxid/settings_spec.rb
@@ -9,7 +9,7 @@ describe ActiveUxid::Settings do
       config.encoder_type = 'ulid'
       config.encoding_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
       config.encoding_length = 26
-      config.encoding_length = 1369136
+      config.encoding_salt = 1369136
     end
   end
 

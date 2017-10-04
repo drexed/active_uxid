eb788bc50628a4952dfbf6bee01363808d018b05
Juan Gomez <j.gomez@drexed.com>
2017-10-03 22:29:25 -0400

    finish hash id

--- lib/active_uxid/hash.rb
+++ lib/active_uxid/hash.rb
@@ -4,44 +4,53 @@ module ActiveUxid
   class Hash < ActiveUxid::Base
 
     def initialize(id)
+      @id = id
     end
 
     def self.encode(id)
       klass = new(id)
-      klass.encode_uxid(id)
+      klass.encode_uxid
     end
 
     def self.decode(id)
       klass = new(id)
-      klass.decode_uxid(id)
+      klass.decode_uxid
     end
 
-    def encode_uxid(id)
-      uxid_encode_chars((id + ENCODING_SALT) << 18)
+    def encode_uxid
+      uxid_encode_chars((@id + ENCODING_SALT) << ENCODING_LENGTH)
     end
 
-    def decode_uxid(id)
-      (uxid_decode_chars(id) >> 18) - ENCODING_SALT
+    def decode_uxid
+      (uxid_decode_chars(@id) >> ENCODING_LENGTH) - ENCODING_SALT
     end
 
     def uxid_encode_chars(id)
-      return ENCODING_CHARS[0] if id.zero?
+      return '0' if id.zero?
+      return nil if id < 0
+
       str = ''
 
       while id > 0
-        str << ENCODING_CHARS[id.modulo(ENCODING_BASE)]
+        str = "#{ENCODING_CHARS[id % ENCODING_BASE]}#{str}"
         id /= ENCODING_BASE
       end
 
-      str.reverse
+      str
     end
 
-    def uxid_decode_chars(str)
-      id = 0
+    def uxid_decode_chars(id)
+      pos = 0
+      num = 0
+      len = id.length - 1
 
-      str.each_char { |char| id = id * ENCODING_BASE + ENCODING_CHARS.index(char) }
+      while pos < id.length
+        pow = ENCODING_BASE ** (len - pos)
+        num += ENCODING_CHARS.index(id[pos]) * pow
+        pos += 1
+      end
 
-      id
+      num
     end
 
   end
--- spec/lib/active_uxid/hash_spec.rb
+++ spec/lib/active_uxid/hash_spec.rb
@@ -1,17 +1,54 @@
 require 'spec_helper'
 
 describe ActiveUxid::Hash do
-  let(:coder) { ActiveUxid::Hash.new }
+  let(:encoder) { ActiveUxid::Hash.new(10) }
+  let(:decoder) { ActiveUxid::Hash.new('q5D8inm0') }
 
   describe '#encode' do
-    it 'to be "6jLK5q0"' do
-      expect(ActiveUxid::Hash.encode(10)).to eq('6jLK5q0')
+    it 'to be "q5D8inm0"' do
+      expect(ActiveUxid::Hash.encode(10)).to eq('q5D8inm0')
     end
   end
 
   describe '#decode' do
     it 'to be 10' do
-      expect(ActiveUxid::Hash.decode('6jLK5q0')).to eq(10)
+      expect(ActiveUxid::Hash.decode('q5D8inm0')).to eq(10)
+    end
+  end
+
+  describe '#encode_uxid' do
+    it 'to be "q5D8inm0"' do
+      expect(encoder.encode_uxid).to eq('q5D8inm0')
+    end
+  end
+
+  describe '#decode_uxid' do
+    it 'to be 10' do
+      expect(decoder.decode_uxid).to eq(10)
+    end
+  end
+
+  describe '#uxid_encode_chars' do
+    it 'to be "n"' do
+      expect(encoder.uxid_encode_chars(23)).to eq('n')
+    end
+
+    it 'to be "0"' do
+      expect(encoder.uxid_encode_chars(0)).to eq('0')
+    end
+
+    it 'to be nil' do
+      expect(encoder.uxid_encode_chars(-1)).to eq(nil)
+    end
+  end
+
+  describe '#uxid_decode_chars' do
+    it 'to be 23' do
+      expect(decoder.uxid_decode_chars('n')).to eq(23)
+    end
+
+    it 'to be 0' do
+      expect(decoder.uxid_decode_chars('0')).to eq(0)
     end
   end
 
